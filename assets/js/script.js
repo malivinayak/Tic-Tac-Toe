7576fed34eecbbe9fcf6302c907f66c4e7de2240
Vinayak Mali <malivinayak012@gmail.com>
2023-05-07 16:36:14 +0530

    init: Tic Tac Toe game

---
+++ assets/js/script.js
@@ -0,0 +1,135 @@
+
+var P1 = true;
+var choice = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+];
+
+function box1() {
+    if (choice[0][0] == "-") {
+        document.getElementById("box1").innerHTML = P1 ? "X" : "O";
+        choice[0][0] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box2() {
+    if (choice[0][1] == "-") {
+        document.getElementById("box2").innerHTML = P1 ? "X" : "O";
+        choice[0][1] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box3() {
+    if (choice[0][2] == "-") {
+        document.getElementById("box3").innerHTML = P1 ? "X" : "O";
+        choice[0][2] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box4() {
+    if (choice[1][0] == "-") {
+        document.getElementById("box4").innerHTML = P1 ? "X" : "O";
+        choice[1][0] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box5() {
+    if (choice[1][1] == "-") {
+        document.getElementById("box5").innerHTML = P1 ? "X" : "O";
+        choice[1][1] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box6() {
+    if (choice[1][2] == "-") {
+        document.getElementById("box6").innerHTML = P1 ? "X" : "O";
+        choice[1][2] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box7() {
+    if (choice[2][0] == "-") {
+        document.getElementById("box7").innerHTML = P1 ? "X" : "O";
+        choice[2][0] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box8() {
+    if (choice[2][1] == "-") {
+        document.getElementById("box8").innerHTML = P1 ? "X" : "O";
+        choice[2][1] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+function box9() {
+    if (choice[2][2] == "-") {
+        document.getElementById("box9").innerHTML = P1 ? "X" : "O";
+        choice[2][2] = P1 ? "X" : "O";
+        checkResult();
+        P1 = !P1;
+    }
+}
+
+function checkResult() {
+    var r1 = 0, r2 = 0;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (choice[i][j] == "X") {
+                r1++;
+            } else if (choice[i][j] == "O") {
+                r2++;
+            }
+        }
+        if (r1 == 3 || r2 == 3) {
+            document.getElementById("result").innerHTML = r1 == 3 ? "Player 1 Win" : "Player 2 Win";
+            for (var x = 1; x <= 9; x++) {
+                const button = document.getElementById("box" + x);
+                button.disabled = true;
+            }
+        }
+        r1 = 0;
+        r2 = 0;
+    }
+
+
+    var d1 = 0, d2 = 0;
+    for (i = 0; i < 3; i++) {
+        if (choice[i][i] == "X") {
+            d1++;
+        } else if (choice[i][i] == "O") {
+            d2++;
+        }
+    }
+    if (d1 == 3 || d2 == 3) {
+        document.getElementById("result").innerHTML = d1 == 3 ? "Player 1 Win" : "Player 2 Win";
+        for (var x = 1; x <= 9; x++) {
+            const button = document.getElementById("box" + x);
+            button.disabled = true;
+        }
+    }
+    d1 = 0;
+    d2 = 0;
+    for (i = 0; i < 3; i++) {
+        if (choice[i][2-i] == "X") {
+            d1++;
+        } else if (choice[i][2-i] == "O") {
+            d2++;
+        }
+    }
+    if (d1 == 3 || d2 == 3) {
+        document.getElementById("result").innerHTML = d1 == 3 ? "Player 1 Win" : "Player 2 Win";
+        for (var x = 1; x <= 9; x++) {
+            const button = document.getElementById("box" + x);
+            button.disabled = true;
+        }
+    }
+}
